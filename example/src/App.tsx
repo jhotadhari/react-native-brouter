a0e07444a2528f614c9a0b677e82843fb609607d
jhotadhari <tellme@waterproof-webdesign.de>
2025-04-11 01:25:01 +0200

    Change example app: Add basic usage of getTrackFromParams

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,20 +1,56 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { multiply } from 'react-native-brouter';
+import { useState } from 'react';
+import { Text, View, Button, ScrollView } from 'react-native';
+import { getTrackFromParams } from 'react-native-brouter';
 
-const result = multiply(3, 7);
+const lonlats = [
+	'-71.047736,-13.950089',
+	'-70.902377,-13.791436',
+	'-70.97545,-13.591525',
+].join( '|' );
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	const [result,setResult] = useState( '' );
+	const [loading,setLoading] = useState( false );
+
+	return (
+		<View style={ {
+			flex: 1,
+			alignItems: 'center'
+		} }>
+
+			<View style={ {
+				height: 150,
+				padding: 20,
+			} }>
+
+				<Text style={ { marginBottom: 10 } } >lonlats: { lonlats }</Text>
+
+				<Button
+					title={ 'get' }
+					onPress={ () => {
+						setLoading( true );
+						getTrackFromParams( {
+							lonlats,
+							trackFormat: 'json',
+							fast: false,
+							v: 'bicycle',
+						} ).then( ( newResult: string ) => {
+							setResult( newResult );
+						} ).catch( ( e: any ) => {
+							setResult( e?.userInfo?.errorMsg );
+						} ).finally( () => setLoading( false ) );
+					} }
+				/>
+
+				{ loading && <Text style={ { marginTop: 10 } }>{ 'loading' }</Text> }
+
+			</View>
+
+			{ result && <ScrollView style={ { paddingHorizontal: 20} }>
+				<Text>{ result }</Text>
+			</ScrollView> }
+
+		</View>
+	);
+};
