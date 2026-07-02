c159e4ba760fdd7f62176955ceafba7f7a868a0e
jhotadhari <tellme@waterproof-webdesign.de>
2026-07-02 20:38:42 +0200

    fix: remove release-kit import from config to avoid tsc issues

--- release.config.ts
+++ release.config.ts
@@ -1,6 +1,4 @@
-import { defineConfig } from 'release-kit';
-
-export default defineConfig({
+export default {
 	repo: 'https://github.com/jhotadhari/react-native-brouter',
 	publish: { npm: true, github: true },
-});
+};
